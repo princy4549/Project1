5d48dc82632daf76f468075abce03018c9d1a443
princy4549 <princy4549@gmail.com>
2025-09-05 16:48:10 +0530

    added navbar and hero

---
+++ src/component/Navigation.jsx
@@ -0,0 +1,16 @@
+const Navigation = () => {
+  return (
+    <nav className="container">
+      <div className="logo">
+        <img src="/images/brand_logo.png" alt="logo" />
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <button>Login</button>
+    </nav>
+  );
+}
+export default Navigation; 
